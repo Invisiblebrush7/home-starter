9eaebf21d3a4002c92e16a62659c6b73feddfabb
Alberto Romano <aromanog1@gmail.com>
2021-01-19 22:41:41 -0600

    Added Views for Devise and added attributes for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    keys = [:shipping_address, :first_name, :last_name, :email, :phone_number]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+  end
 end
---
+++ db/migrate/66666666666668_add_last_name_to_users.rb
@@ -0,0 +1,7 @@
+class AddLastNameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :last_name, :string
+    add_column :users, :shipping_address, :string
+    add_column :users, :phone_number, :integer
+  end
+end
