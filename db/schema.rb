14a5e3423880fc5ee00df37a8f14d25ac564a920
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-01-21 22:58:28 -0500

    addeed - the validation for the models, booking reference to reviews, route changes to include shallow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6666_66_66_666668) do
+ActiveRecord::Schema.define(version: 6666_66_66_666669) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,9 @@ ActiveRecord::Schema.define(version: 6666_66_66_666668) do
     t.bigint "booking_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "bundles_offer_id", null: false
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["bundles_offer_id"], name: "index_reviews_on_bundles_offer_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -90,4 +92,5 @@ ActiveRecord::Schema.define(version: 6666_66_66_666668) do
   add_foreign_key "bundles_offers", "users"
   add_foreign_key "furnitures", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "bundles_offers"
 end
