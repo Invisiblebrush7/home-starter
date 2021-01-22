1290ce55041cd4fa65e1741e6dd29c345b2a258e
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-01-21 23:53:14 -0500

    model validation failed when pushed to heroku, corrected that

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -2,7 +2,7 @@ class Furniture < ApplicationRecord
   belongs_to :user
   has_many :bundled_furnitures
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :description, presence: true, { maximum: 200,
+  validates :description, presence: true, length: { maximum: 200,
     too_long: "%{count} characters is the maximum allowed" }
   validates :dimensions, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
