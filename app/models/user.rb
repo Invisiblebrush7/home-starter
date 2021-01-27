e8d8aa29d630643ed5662908058717427122fc36
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-01-26 23:54:44 -0500

    removed some of the validation in the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true, uniqueness: { scope: :last_name }, length: { minimum: 3 }
   validates :last_name, presence: true, uniqueness: true, length: { minimum: 3 }
-  validates :password, length: { in: 6..20 }
-  validates :shipping_address, presence: true
-  validates :phone_number, presence: true, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/, message: "must be in xxx-xxx-xxxx format." }
+  # validates :password, length: { in: 6..20 }
+  # validates :shipping_address, presence: true
+  # validates :phone_number, presence: true, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/, message: "must be in xxx-xxx-xxxx format." }
   validates :email, format: { with: /\A.*@.*\.com\z/ }
   has_many :furnitures
   has_many :bookings
