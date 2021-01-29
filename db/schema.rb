f44082066ef9c2c6e0a1fb07903608abf3a943b2
Alberto Romano <aromanog1@gmail.com>
2021-01-28 20:56:15 -0600

    Fixed double navbar issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 6666_66_66_666676) do
+ActiveRecord::Schema.define(version: 6666_66_66_666677) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
