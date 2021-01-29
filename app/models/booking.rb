b35d5dfd2c9f0a5e01a60320c58cccad87debf2d
Alberto Romano <aromanog1@gmail.com>
2021-01-29 13:14:54 -0600

    Validator thing work

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,19 @@
+class AvailableDatesValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    bookings = Booking.where(["bundles_offer_id =?", record.bundles_offer_id])
+    date_ranges = bookings.map { |b| b.start_date..b.end_date }
+
+    date_ranges.each do |range|
+      if range.include? value
+        record.errors.add(attribute, "not available")
+      end
+    end
+  end
+end
+
 class Booking < ApplicationRecord
   belongs_to :bundles_offer
   belongs_to :user
+
+  validates :start_date, :end_date, available_dates: true
 end
